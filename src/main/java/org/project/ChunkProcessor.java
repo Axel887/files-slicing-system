dec1d3ff61a742432c13c8f75096058c9d77f85f
johannes60-sk <garilsenan@gmail.com>
2025-02-19 16:28:09 +0100

    feat: add Snappy compression support and enhance chunk processing logic

--- src/main/java/org/project/ChunkProcessor.java
+++ src/main/java/org/project/ChunkProcessor.java
@@ -3,6 +3,7 @@ package org.project;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import org.xerial.snappy.Snappy;
 
 public class ChunkProcessor {
     private final ChunkStorage chunkStorage;
@@ -16,16 +17,36 @@ public class ChunkProcessor {
 
     public void processFile(File file) throws IOException {
         List<byte[]> chunks = fileChunker.getChunks(file);
-
+        int chunkCount = 1;
         for (byte[] chunk : chunks) {
             String chunkHash = Blake3Hasher.hashChunk(chunk);
-            if (!chunkStorage.contains(chunkHash)) {
-                chunkStorage.storeChunk(chunkHash, chunk);
-            } else {
-                System.out.println("Chunk dupliqué détecté : " + chunkHash);
-            }
-        }
 
-        chunkStorage.displayChunks();
+            if (!chunkStorage.contains(chunkHash)) {
+                displayChunk(chunkCount, chunkHash, chunk);
+
+                // Compression du chunk avec Snappy
+                // byte[] compressedChunk = Snappy.compress(chunk);
+
+                // compression du chuck avec LZ4
+                byte[] compressedChunk = compressor.compressChunk(chunk);
+
+                // Stocker le chunk compressé
+                chunkStorage.storeChunk(chunkHash, compressedChunk);
+
+                // Affichage des tailles
+                System.out.println("Chunk #" + chunkCount + " info taille original et compressé :");
+                System.out.println("  - Taille originale : " + chunk.length + " bytes");
+                System.out.println("  - Taille compressée : " + compressedChunk.length + " bytes");
+            } else {
+                System.out.println("Chunk #" + chunkCount + " déjà existant (doublon détecté).");
+            }
+
+            chunkCount++;
+        }
+    }
+
+    private void displayChunk(int chunkNumber, String chunkId, byte[] chunkData) {
+        System.out.println(
+                "Chunk (" + chunkNumber + ")" + " : " + chunkId + ", Size: " + chunkData.length + " bytes");
     }
 }
