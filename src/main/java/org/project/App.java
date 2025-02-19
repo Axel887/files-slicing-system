d13c2566dea5f477d26bf3ec90ff5d203bc2f01c
Mehdi Mahmoudi <mehdimahmoudi@MacBook-Pro-de-Mehdi.local>
2025-02-19 11:32:42 +0100

    fix: polynomial fixed and add loop

--- src/main/java/org/project/App.java
+++ src/main/java/org/project/App.java
@@ -6,21 +6,36 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Entrez le chemin du fichier à découper : ");
-        String filePath = scanner.nextLine();
-        scanner.close();
+        boolean closeProgram = false;
+        FileChunker fileChunker = new FileChunker();
+        Scanner scanner;
 
-        File file = new File(filePath);
-        if (!file.exists()) {
-            System.out.println("Erreur : Le fichier spécifié n'existe pas.");
-            return;
-        }
+        do {
+            scanner = new Scanner(System.in);
+            System.out.print("Entrez le chemin du fichier à découper : ");
+            String filePath = scanner.nextLine();
 
-        try {
-            new FileChunker().chunkFile(file);
-        } catch (IOException e) {
-            System.err.println("Erreur lors du découpage du fichier : " + e.getMessage());
-        }
-    }
+            File file = new File(filePath);
+            if (!file.exists()) {
+                System.out.println("Erreur : Le fichier spécifié n'existe pas.");
+                return;
+            }
+
+            try {
+                fileChunker.chunkFile(file);
+            } catch (IOException e) {
+                System.err.println("Erreur lors du découpage du fichier : " + e.getMessage());
+            }
+
+            scanner = new Scanner(System.in);
+            System.out.print("Continuer (1) ou stopper (tous caracteres) : ");
+            String choiceValue = scanner.nextLine();
+
+            if (!choiceValue.equals("1")) {
+                closeProgram = true;
+            }
+        } while (!closeProgram);
+
+    scanner.close();
+  }
 }
