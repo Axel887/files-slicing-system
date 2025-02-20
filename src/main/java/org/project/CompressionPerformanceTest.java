801057a752457afa1140a19483d77b98afe76818
Mehdi-Mah <mdi.mahmoudi@gmail.com>
2025-02-20 12:37:21 +0100

    fix: performance size chunks compressed

--- src/main/java/org/project/CompressionPerformanceTest.java
+++ src/main/java/org/project/CompressionPerformanceTest.java
@@ -14,7 +14,7 @@ public class CompressionPerformanceTest {
 
         public void runTest(File file) throws IOException {
                 byte[] fileData = Files.readAllBytes(file.toPath());
-
+                int  compressedChunkSize = getSizeCompressedChunks(file);
                 // Compression globale
                 long startGlobal = System.nanoTime();
                 byte[] compressedGlobal = compressor.compressChunk(fileData);
@@ -30,7 +30,27 @@ public class CompressionPerformanceTest {
                 long chunkTimeMs = (endChunks - startChunks) / 1_000_000;
 
                 System.out.println(
-                                "Compression par chunk : " + chunkTimeMs + " ms, taille totale : " + totalChunkSize
+                                "Compression par chunk : " + chunkTimeMs + " ms, taille totale : " + compressedChunkSize
                                                 + " octets");
+        }
+
+        private int getSizeCompressedChunks(File file) throws IOException {
+                List<byte[]> chunks = fileChunker.getChunks(file);
+                int compressedChunkSize = 0;
+
+                for (byte[] chunk : chunks) {
+                        // compression du chuck avec LZ4
+                        // byte[] compressedChunk = compressor.compressChunk(chunk);
+
+                        // Compression de chunk avec Zstd
+                        byte[] compressedChunk = ZstdCompressor.compressChunk(chunk);
+                        compressedChunkSize += compressedChunk.length;
+
+                        // Compression du chunk avec Snappy
+                        // byte[] compressedChunk = Snappy.compress(chunk);
+
+                }
+
+                return compressedChunkSize;
         }
 }
